7df56cf34c3418f0c81128a2eb4001d4140a46b2
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-12-30 23:50:06 +0900

    Introduce Crashlytics

--- app/src/main/java/io/github/shoma2da/android/aliceinandroid/AliceApplication.kt
+++ app/src/main/java/io/github/shoma2da/android/aliceinandroid/AliceApplication.kt
@@ -2,6 +2,8 @@ package io.github.shoma2da.android.aliceinandroid
 
 import android.app.Application
 import co.meyasuba.android.sdk.Meyasubaco
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by shoma2da on 2015/12/30.
@@ -10,5 +12,6 @@ class AliceApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Meyasubaco.setApiKey(this, "e0da968afdf2042b1620b104587070d746b6402d2f4d05d17caec2a9f843575a")
+        Fabric.with(this, Crashlytics());
     }
 }
