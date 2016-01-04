bf03ac354897f6c0213e9ef1779183bcc7b14ec2
shoma2da <marcos.shoichi.matsuda@gmail.com>
2016-01-05 08:42:32 +0900

    Prepare all stories

--- app/src/main/java/io/github/shoma2da/android/aliceinandroid/model/Stories.kt
+++ app/src/main/java/io/github/shoma2da/android/aliceinandroid/model/Stories.kt
@@ -17,12 +17,12 @@ class Stories {
                 Story(4, "うさぎ、小さなビルをおくりこむ", R.layout.layout_4th),
                 Story(5, "いもむしの忠告", R.layout.layout_5th),
                 Story(6, "ぶたとコショウ", R.layout.layout_6th),
-                Story(7, "キチガイお茶会", R.layout.layout_prologue),
-                Story(8, "女王さまのクロケー場", R.layout.layout_prologue),
-                Story(9, "にせウミガメのお話", R.layout.layout_prologue),
-                Story(10, "ロブスターのカドリーユおどり", R.layout.layout_prologue),
-                Story(11, "タルトをぬすんだのはだれ？", R.layout.layout_prologue),
-                Story(12, "アリスのしょうこ", R.layout.layout_prologue)
+                Story(7, "キチガイお茶会", R.layout.layout_7th),
+                Story(8, "女王さまのクロケー場", R.layout.layout_8th),
+                Story(9, "にせウミガメのお話", R.layout.layout_9th),
+                Story(10, "ロブスターのカドリーユおどり", R.layout.layout_10th),
+                Story(11, "タルトをぬすんだのはだれ？", R.layout.layout_11th),
+                Story(12, "アリスのしょうこ", R.layout.layout_12th)
         )
 
         fun reset(context:Context) {
